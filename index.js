74985679df7c6c2a8abee0b79f632ae34358aad6
cassio caue <cassio3620@gmail.com>
2019-10-21 20:17:30 -0300

    adicionando methodo post

--- index.js
+++ index.js
@@ -15,13 +15,35 @@ function checkProjectExists(req, res, next) {
   if (!project) {
     return res.status(400).json({ error: 'Project not foud' })
   }
-  
   return next();
 }
 
+function logRequest(req, res, next) {
+  console.count("Número de requisições")
 
-app.get('/', (req, res) => {
-  return res.json({ message: " ok " })
+  return next();
+}
+
+app.use(logRequests);
+
+
+app.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+  const project = {
+    id,
+    title,
+    tasks:[]
+  };
+
+  project.push(project);
+
+  return res.json(project);
+
+})
+
+
+app.get('/project', (req, res) => {
+  return res.json(projects)
 })
 
 
